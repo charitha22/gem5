84ddb4c8140f5b5bc4774a1fc98eadede9a03fd1
Charitha Saumya <cgusthin@purdue.edu>
2019-11-28 17:32:45 -0500

    drrip run scripts

---
+++ run_drrip.sh
@@ -0,0 +1,18 @@
+./build/ALPHA/gem5.opt \
+--debug-flags=CacheReplPolicy \
+-d drriprp_out \
+configs/example/se.py \
+--benchmark=mcf \
+--cpu-type=DerivO3CPU \
+--caches --l2cache \
+--l2_size='512kB' \
+--l2_assoc=8 \
+--l1d_size='16kB' \
+--l1i_size='16kB' \
+--cache_repl_policy='DRRIPRP' \
+-F 1000000000 \
+-s 100000000 \
+-W 100000000 \
+-I 250000000
+
+echo "Done" | mailx -s "Job Finished" cgusthin@purdue.edu
